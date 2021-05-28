23e63170e4dfb805aef95206d5ad6472d2c362c1
Austin Waldsmith <austin.waldsmith@gmail.com>
2021-05-27 23:04:26 -0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(TextPlugin, ScrollToPlugin, ScrollTrigger);
